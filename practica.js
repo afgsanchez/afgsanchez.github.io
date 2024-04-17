f1d83c9b1c7eb3498cbdb2dd255827e1bb060cac
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2024-04-17 23:04:44 +0200

    Version v1.0.0

---
+++ practica.js
@@ -0,0 +1,11 @@
+var libro = {
+    titulo:'Las Legiones Malditas', autor:'Santiago Posterguillo', informacion: function (){return this.titulo + " escrito por " + this.autor; }
+}
+console.log(typeof libro.informacion);
+console.log("---------------------------");
+// ------------------------------------
+
+<script type="text/javascript">
+   let intentos = [1, 5, 10, 12]
+   console.log(intentos[4])
+</script>
